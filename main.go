4869292cbf4fd49971862bb13eceb0f186ee7bb0
TahshinSharon <atsharon88@gmail.com>
2024-12-24 14:35:16 +0600

    Tahshin at 2.35

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"unicode"
-)
+import "fmt"
 
 func main() {
-	input := "Madam,I'mAdam"
-	normalized := ""
-	for _, r := range input {
-		if unicode.IsLetter(r) {
-			normalized += string(unicode.ToLower(r))
-		}
-	}
-	isPalindrome := true
-	for i := 0; i < len(normalized)/2; i++ {
-		if normalized[i] != normalized[len(normalized)-1-i] {
-			isPalindrome = false
-			break
-		}
-	}
-	if isPalindrome {
-		fmt.Println("It's a palindrome!")
-	} else {
-		fmt.Println("It's not a palindrome.")
-	}
+	fmt.Println("Hello World")
 }
